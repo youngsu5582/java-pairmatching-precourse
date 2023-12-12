bd0d74c02f5439ec2a839177895da646c41dc188
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-12 17:27:58 +0900

    feat : (PariCatalog) Domain 생성
    Record로 선언
    Course , Level , missionName을 가지고 있는다.
    이에 따른 테스트 코드 추가

---
+++ src/main/java/pairmatching/domain/PairCatalog.java
@@ -0,0 +1,4 @@
+package pairmatching.domain;
+
+public record PairCatalog(Course course,Level level,String missionName) {
+}
---
+++ src/test/java/pairmatching/domain/PairCatalogTest.java
@@ -0,0 +1,25 @@
+package pairmatching.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PairCatalogTest {
+    @Test
+    @DisplayName("Course 와 Level 과 미션명을 통해 페어 카탈로그를 생성한다.")
+    void createPairCatalog(){
+        //Given
+        Course course = Course.BACKEND;
+        Level level = Level.LEVEL4;
+        String missionName = "성능개선";
+
+        //When
+        PairCatalog pairCatalog = new PairCatalog(course,level,missionName);
+        PairCatalog pairCatalog1 = new PairCatalog(course,level,missionName);
+
+        //Then
+        //해당 부분 에서 동일함 을 검증 하는 이유는
+        //Catalog 를 통해 , 결과를 검색 하므로 검증
+        Assertions.assertEquals(pairCatalog,pairCatalog1);
+    }
+}
