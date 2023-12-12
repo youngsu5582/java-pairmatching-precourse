1aab6d271972cd3155e85ae94d1bf02f21ca6b5f
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-12 17:18:12 +0900

    feat : (Mission) Domain 생성
    Record 로 선언
    Level , missionName 을 가지고 있는다.
    이에 따른 테스트 코드 추가

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,4 @@
+package pairmatching.domain;
+
+public record Mission(Level level,String missionName) {
+}
---
+++ src/test/java/pairmatching/domain/MissionTest.java
@@ -0,0 +1,23 @@
+package pairmatching.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MissionTest {
+    @Test
+    @DisplayName("레벨 과 미션명을 통해 미션을 생성한다.")
+    void createMission(){
+        //Given
+        Level level = Level.LEVEL1;
+        String missionName = "지하철노선도";
+
+        //When
+        Mission mission = new Mission(level,missionName);
+
+        //Then
+        Assertions.assertInstanceOf(Mission.class,mission);
+        Assertions.assertEquals(mission.level(),level);
+        Assertions.assertEquals(mission.missionName(),missionName);
+    }
+}
