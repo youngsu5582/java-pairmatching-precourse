552c3140f82241f0792070699e359d83799fc9bc
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-12 19:53:19 +0900

    feat : (View) printPreviewMessage 추가
    과정 미션 예시를 출력 해주는 printPreviewMessage 추가
    하위 printCourseMessage , printMissionMessage 추가

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,5 +1,13 @@
 package pairmatching.view;
 
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
+import pairmatching.domain.MissionBoard;
+import pairmatching.view.message.OutputViewMessage;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class OutputView {
     private static OutputView INSTANCE=null;
     public static synchronized OutputView getInstance(){
@@ -14,5 +22,30 @@ public class OutputView {
 
     private void printMessage(String message) {
         System.out.println(message);
+    }
+    public void printPreviewMessage(MissionBoard missionBoard){
+        printMessage(OutputViewMessage.PREVIEW_MESSAGE.getMessage());
+
+        printCourseMessage();
+        printMissionMessage(missionBoard);
+
+        printMessage(OutputViewMessage.PREVIEW_MESSAGE.getMessage());
+        printMessage(OutputViewMessage.SELECT_MESSAGE.getMessage());
+        printMessage(OutputViewMessage.EXAMPLE_MESSAGE.getMessage());
+    }
+
+
+    private void printCourseMessage(){
+        List<String> courseNameList = Arrays.stream(Course.values()).map(course -> course.getName()).toList();
+        String courseNameString = String.join(" | ",courseNameList);
+        printMessage(OutputViewMessage.COURSE_MESSAGE.getFormattedMessage(courseNameString));
+    }
+    private void printMissionMessage(MissionBoard missionBoard){
+        printMessage(OutputViewMessage.MISSION_MESSAGE.getMessage());
+        for(Level level:Level.values()){
+            List<String> missionNameList = missionBoard.getMissionNameList(level);
+            String missionNameString = String.join(" | ",missionNameList);
+            printMessage(OutputViewMessage.LEVEL_MESSAGE.getFormattedMessage(level.getName(),missionNameString));
+        }
     }
 }
--- src/main/java/pairmatching/view/message/OutputViewMessage.java
+++ src/main/java/pairmatching/view/message/OutputViewMessage.java
@@ -1,7 +1,12 @@
 package pairmatching.view.message;
 
 public enum OutputViewMessage {
-    ;
+    PREVIEW_MESSAGE("#############################################"),
+    COURSE_MESSAGE("과정: %s"),
+    MISSION_MESSAGE("미션:"),
+    LEVEL_MESSAGE("  - %s: %s"),
+    SELECT_MESSAGE("과정, 레벨, 미션을 선택하세요."),
+    EXAMPLE_MESSAGE("ex) 백엔드, 레벨1, 자동차경주");
     private final String message;
 
     OutputViewMessage(String message) {
